bf5495c5997d42a3cfad1aa1d646d72da0b6eab1
Adam Kapelner <kapelner@gmail.com>
2016-09-19 16:03:40 -0400

    solids fixed

--- GemIdentTools/Geometry/Solids.java
+++ GemIdentTools/Geometry/Solids.java
@@ -23,6 +23,7 @@
 */
 
 package GemIdentTools.Geometry;
+
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,18 +31,18 @@ import java.util.Comparator;
 import java.util.HashMap;
 
 /**
- * Provides masks of filled circles of various radii 
- * similar to the {@link Rings Rings} class
+ * Provides masks of filled circles of varius radii 
+ * similiar to the {@link Rings Rings} class
  * which provides masks of unfilled circles
  * 
  * @author Adam Kapelner
  */
 public class Solids {
 	
-	/** the maximum solid's radius for the initial build */
+	/** the maxium solid's radius for the initial build */
 	public static final int INIT_MAX_SOLID=50;
 	/** the mapping from radius to the solid itself stored as a list of {@link java.awt.Point Point} objects */
-	private static HashMap<Integer, ArrayList<Point>> solidLists;
+	private static HashMap<Integer,ArrayList<Point>> solidLists;
 	
 	/** To construct the static object, call Build */
 	static {
@@ -50,39 +51,31 @@ public class Solids {
 	/** Build the initial solids for radius = {0, 1, . . ., INIT_MAX_SOLID} */
 	public static void Build(){
 		//init the mapping
-		solidLists=new HashMap<Integer, ArrayList<Point>>(INIT_MAX_SOLID);
+		solidLists=new HashMap<Integer,ArrayList<Point>>(INIT_MAX_SOLID);
 		//init the first solid
 		ArrayList<Point> zeroList=new ArrayList<Point>();
-		solidLists.put(0, zeroList);
+		zeroList.add(new Point(0,0));
+		solidLists.put(0,zeroList);
 		//init all other solids
 		for (int r=1;r<=INIT_MAX_SOLID;r++)
 			GenerateSolid(r);
 	}	
-
-	
 	/**
 	 * Generate a solid of radius r using an inefficient algorithm
 	 * 
 	 * @param r			the radius of the solid to be generated
-	 * @param theta		the angle offset from 0 degrees (in radians)
 	 * @return			the solid as a list of coordinates
 	 */
 	private static ArrayList<Point> GenerateSolid(int r){
-		ArrayList<Point> solid_list = solidLists.get(r);
-		if (solid_list == null){
-			int rsq = r * r;
-			ArrayList<Point> solidList=new ArrayList<Point>();
-			for (int x=-r;x<=r;x++)
-				for (int y=-r;y<=r;y++)
-					if (x*x + y*y <= rsq)
-						solidList.add(new Point(x,y));
-			solidLists.put(r, new ArrayList<Point>());
-		}
-		return solid_list;
-		
-
-	}	
-	
+		int rsq=r*r;
+		ArrayList<Point> solidList=new ArrayList<Point>();
+		for (int x=-r;x<=r;x++)
+			for (int y=-r;y<=r;y++)
+				if (x*x + y*y <= rsq)
+					solidList.add(new Point(x,y));
+		solidLists.put(r, sortByDistance(solidList));
+		return solidList;
+	}
 	/**
 	 * This sorts points by their distance from the origin
 	 * 
@@ -105,9 +98,9 @@ public class Solids {
 			double d2 = Math.pow(t2.x, 2) + Math.pow(t2.y, 2);
 			return (int)(d1 - d2);
 		}
-		
-		
 	}
+	
+		
 	/**
 	 * Return a solid of radius r. If the solid is not yet generated, 
 	 * it will autogenerate, cache it, and return it
@@ -116,7 +109,7 @@ public class Solids {
 	 * @return		the solid
 	 */
 	public static ArrayList<Point> getSolid(int r){
-		ArrayList<Point> solid = solidLists.get(r);
+		ArrayList<Point> solid=solidLists.get(r);
 		if (solid == null)
 			return GenerateSolid(r);
 		else
@@ -130,13 +123,11 @@ public class Solids {
 	 * @param to		the center of the solid
 	 * @return			the solid desired
 	 */
-	public static ArrayList<Point> GetPointsInSolidUsingCenter(int r, Point to){
-		ArrayList<Point> solid = getSolid(r);
+	public static ArrayList<Point> GetPointsInSolidUsingCenter(int r,Point to){
+		ArrayList<Point> solid=getSolid(r);
 		ArrayList<Point> points=new ArrayList<Point>(solid.size());
 		for (Point t:solid)
 			points.add(new Point(to.x+t.x,to.y+t.y));
 		return points;
 	}
-		
-	
 }
